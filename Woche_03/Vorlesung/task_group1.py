ab3564a02471d5b5a83756f2b041523704ada926
JoScha93 <64128250+JoScha93@users.noreply.github.com>
2022-04-19 18:58:47 +0200

    added files: live coding tasks for week 3

---
+++ Woche_03/Vorlesung/task_group1.py
@@ -0,0 +1,11 @@
+"""
+Live coding task - first group
+"""
+
+
+def length_of_all_elements(lst):
+    """Hier Ihre Implementierung"""
+    pass
+
+
+length_of_all_elements(["Michael", "Anna", "Ali"])  # -> [7, 4, 3]
---
+++ Woche_03/Vorlesung/task_group2.py
@@ -0,0 +1,11 @@
+"""
+Live Coding task - second group
+"""
+
+
+def starts_with_m(lst):
+    """Hier Ihre Implementierung"""
+    pass
+
+
+starts_with_m(["Michael", "Anna", "Ali"]) # -> ["Michael"]
