2beaf0e31b35f1bb9cae73fa343081a515788bdc
JoScha93 <64128250+JoScha93@users.noreply.github.com>
2022-04-20 12:10:31 +0200

    updated files: live coding tasks + solutions

--- Woche_03/Vorlesung/task_group1.py
+++ Woche_03/Vorlesung/task_group1.py
@@ -1,11 +1,25 @@
 """
-Live coding task - first group
+Live coding task + solution
 """
 
 
+# Solution using for loops
 def length_of_all_elements(lst):
-    """Hier Ihre Implementierung"""
-    pass
+    return_list = []
+    for name in lst:
+        return_list.append(len(name))
+    return return_list
 
 
-length_of_all_elements(["Michael", "Anna", "Ali"])  # -> [7, 4, 3]
+print(length_of_all_elements(["Michael", "Anna", "Ali"]))  # -> [7, 4, 3]
+
+
+# Solution using map
+def length_of_all_elements_map(lst):
+    # type-casting the return value of map function to list
+    return list(map(len, lst))
+
+
+# Solution using list comprehensions
+def length_of_all_elements_map(lst):
+    return [len(name) for name in lst]
--- Woche_03/Vorlesung/task_group2.py
+++ Woche_03/Vorlesung/task_group2.py
@@ -1,11 +1,30 @@
 """
-Live Coding task - second group
+Live Coding task + solution
 """
 
 
+# Solution with for loops
 def starts_with_m(lst):
-    """Hier Ihre Implementierung"""
-    pass
+    return_list = []
+    for name in lst:
+        if name[0].upper() == "M":
+            return_list.append(name)
+    return return_list
 
 
-starts_with_m(["Michael", "Anna", "Ali"]) # -> ["Michael"]
+print(starts_with_m(["Michael", "Anna", "Ali"])) # -> ["Michael"]
+
+
+# Filter solution
+def filter_function(word):
+    return word[0].upper() == "M"
+
+
+def start_with_m_filter(lst):
+    # type-casting the return value of filter function to list
+    return list(filter(filter_function, lst))
+
+
+# List comprehensions
+def start_with_m_lc(lst):
+    return [name for name in lst if name[0].upper() == "M"]
